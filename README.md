16a5683224c938aef7c423d5a271c666782e9bf4
Vincent Favre-Nicolin <vincefn@gmail.com>
2015-08-18 12:01:49 +0200

    pyobjcryst auto test and requirements

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # CrystCompSchool
-Tutorials for crystallographic computing school
+Tutorials for the crystallographic computing school
+
+
+# Tutorial #1: (py)ObjCryst++ (1h30)
+The ObjCryst++ C++ library (http://objcryst.sf.net) was developed for ab initio structure solution from powder diffraction. In this tutorial we will first give a quick tour of the C++ library capabilities. Then we will use the python interface to exploit this library, including: creating or importing crystal structure from CIF, calculating & displaying powder patterns, performing a global optimization.
+
+Keywords: python/C++, small molecules/inorganic structures, powder diffraction
+Requirements: install python+libobjcryst+pyobjcryst+matplotlib, optional install of Fox, good python knowledge
+
+
+# Tutorial #2: global optimization (1h to 1h30 ?)
+Finding the numerical solution to a problem can be tricky when the 'parameter space' to explore is large. In this tutorial we focus on 'well-defined' problems where the parametrization is known (e.g. a fixed number of atoms) but the value range of each parameter is very large. We will illustrate a few algorithms (simulated annealing, parallel tempering,differential evolution) which can be used to tackle this type of problems
+
+Requirements: python/numpy/matplotlib, some python experience
+
+
+# Tutorial #3: quick introduction to GPU computing with pyOpenCL and PyNX (40')
+GPU use for general computing has been developed a lot during the past 10 years, due to the significant speedup (up to 1000x) it can provide vs traditional microprocessing computing. After a quick introduction about GPU computing, the notion of massively parallel calculations and CUDA/OpenCL, we will try simple examples using the PyNX library which allows fast calculations of structure factors.
+
+Requirements: pyOpenCL (or pyCUDA), PyNX
+
+# Tutorial #4: quick introduction to databases, SQL queries, and the Crystallographic Open Database (30')
+Databases are often perceived as complicated by computing scientists. This quick tutorial will show that accessing (and even creating) databases is in fact trivial. We will give a very quick introduction to SQL queries, and how to search and retrieve data from the Crystallographic Open Database (www.crystallography.net/) as an example.
+
+
+Requirements: python with mysql, sqlite modules, mysql, internet access. Beginners only
---
+++ test.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 09:25:35 2015
+
+@author: vincent
+"""
+
+import pyobjcryst
+import numpy as np
+import platform
+import os
+
+#Get the data from IUCr journals (once)
+if True:
+  WGET="wget -q -O-"
+  if platform.uname[0]=='Darwin': WGET="curl -O"
+  # http://scripts.iucr.org/cgi-bin/paper?dt3034
+  os.system("%s 'http://scripts.iucr.org/cgi-bin/sendcif?dt3034sup1' > dt3034.cif"%WGET)
+  os.system("%s 'http://journals.iucr.org/c/issues/2015/09/00/dt3034/dt3034Isup2.hkl' > dt3034Isup2.hkl"%WGET)
+  # http://scripts.iucr.org/cgi-bin/paper?hx5075
+  os.system("%s 'http://scripts.iucr.org/cgi-bin/sendcif?hx5075sup1' > hx5075.cif"%WGET)
+  os.system("%s 'http://scripts.iucr.org/cgi-bin/sendsupfiles?hx5075&file=hx5075sup9.txt' > hx5075sup9.txt"%WGET)
+
+c=pyobjcryst.crystal.CreateCrystalFromCIF(open("dt3034.cif"),True,True)
+
+d=pyobjcryst.diffractiondatasinglecrystal.DiffractionDataSingleCrystal(c)
+d.ImportCIF("dt3034Isup2.hkl")
+d.PrintObsCalcData()
+print(d.GetRw())
+d.SetWavelength(0.71073)
+print(d.GetRw())
+
+#print(c.GetNbScatterer())
+#c.ConnectAtoms()
+print(c.GetNbScatterer())
+m=c.GetScatterer(0)
+print(m.GetFormula())
+#plot(d.GetSinThetaOverLambda(),d.GetIobs()-d.GetIcalc())
+
+
+###################
+
+c=pyobjcryst.crystal.CreateCrystalFromCIF(open("hx5075.cif"),True,True)
+c.GetScatterer(0).GetFormula()
+p=pyobjcryst.powderpattern.PowderPattern()
+p.ImportPowderPattern2ThetaObsSigma("hx5075sup9.txt")
+x=p.GetPowderPatternX()
+
+# Background
+bx=np.linspace(x.min(),x.max(),20)
+by=np.zeros(bx.shape)
+
+b=pyobjcryst.powderpatternbackground.PowderPatternBackground()
+b.SetInterpPoints(bx,by)
+p.AddPowderPatternComponent(b)
+b.Print()
+b.UnFixAllPar()
+b.OptimizeBayesianBackground()
+
+pd=pyobjcryst.powderpatterndiffraction.PowderPatternDiffraction()
+pd.SetCrystal(c)
+
+c.GetNbOption()
+c.GetOption(0).GetName()
+c.GetOption(1).GetName()
+c.GetOption(1).GetChoiceName(0)
+c.GetOption(1).GetChoiceName(1)
+c.GetOption(1).SetChoice(0)
+
+
+p.AddPowderPatternComponent(pd)
+p.SetWavelength(0.801203)
+p.Prepare()
+pd.SetReflectionProfilePar(pyobjcryst.powderpatterndiffraction.ReflectionProfileType.PROFILE_PSEUDO_VOIGT,0.0000001)
+p.FitScaleFactorForIntegratedRw()
+
+from pylab import plot,pi
+plot(p.GetPowderPatternX()*180/pi,p.GetPowderPatternObs())
+plot(p.GetPowderPatternX()*180/pi,p.GetPowderPatternCalc())
+
+m=pyobjcryst.montecarloobj.MonteCarloObj()
+m.AddRefinableObj(c)
+m.AddRefinableObj(p)
+m.Optimize(100000)
+
+
+# LSQ
+lsq=pyobjcryst.lsqnumobj.LSQNumObj()
+
